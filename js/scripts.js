98367cf09323fab68e387205e2acb0e2a1856281
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2023-12-19 17:10:25 +0100

    Click

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,26 @@
     Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 */
+
+const startButton = document.getElementById('start-button');
+console.log('startButton', startButton, typeof startButton);
+
+startButton.addEventListener('click', function() {
+    //console.log('bottone click');
+
+    const grid = document.querySelector('.grid');
+    console.log('grid', grid, typeof grid);
+
+    for (let i = 1; i<=100; i++) {
+        const cell = document.createElement('div');
+        cell.innerHTML = i; 
+        cell.classList.add('cell');
+        cell.addEventListener('click', function() {
+            //console.log('Click cella');
+            cell.classList.add('click');
+            console.log(cell.innerHTML);
+        })
+        grid.append(cell);
+    }
+
+})
